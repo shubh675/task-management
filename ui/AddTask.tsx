cbb016e2aff67b751e85e723044f4ad268c1311d
Shubham Gehlot <shubhamgehlot675@gmail.com>
2023-11-11 13:53:28 +0530

    fixed the issue not Add Task And And Task list not closing when clicked outside

--- ui/AddTask.tsx
+++ ui/AddTask.tsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import api from "@trpc/client-side/react";
 import useSingleBoardState from "@store/useSingleBoardState";
 type TypeAddTaskProps = {
   taskListId: string;
-  userId:string;
+  userId: string;
 };
-const AddTask = ({ taskListId,userId }: TypeAddTaskProps) => {
+const AddTask = ({ taskListId, userId }: TypeAddTaskProps) => {
   const [addingTask, setAddingTask] = useState<boolean>(false);
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const addTaskContainerRef = useRef<HTMLDivElement | null>(null);
   const addTaskMutation = api.task.createTask.useMutation();
   const addTaskToTaskList = useSingleBoardState(
     (state) => state.addTaskToTaskList,
@@ -18,8 +19,10 @@ const AddTask = ({ taskListId,userId }: TypeAddTaskProps) => {
     if (
       inputRef.current?.value === undefined ||
       !inputRef ||
-      inputRef.current.value.length === 0
+      inputRef.current.value.length === 0 ||
+      !inputRef.current.value
     ) {
+      setAddingTask(false);
       return;
     } else if (inputRef.current?.value && inputRef.current.value.length === 0) {
       setAddingTask(false);
@@ -36,11 +39,31 @@ const AddTask = ({ taskListId,userId }: TypeAddTaskProps) => {
       },
     );
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        addingTask &&
+        inputRef.current &&
+        addTaskContainerRef.current &&
+        !inputRef.current.contains(event.target as Node) &&
+        !addTaskContainerRef.current.contains(event.target as Node)
+      ) {
+        setAddingTask(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [addingTask]);
   return (
     <div>
       {" "}
       {addingTask ? (
-        <div className="space-y-2 mt-4">
+        <div className="mt-4 space-y-2">
           <input
             type="text"
             placeholder="Add a task"
@@ -50,13 +73,15 @@ const AddTask = ({ taskListId,userId }: TypeAddTaskProps) => {
           <div className="flex space-x-2">
             <button
               onClick={handleAddTask}
-              className="w-1/2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+              className="w-1/2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 disabled:cursor-not-allowed disabled:opacity-50"
+              disabled={addTaskMutation.isLoading}
             >
               Add Task
             </button>
             <button
-              onClick={()=>setAddingTask(false)}
-              className="w-1/2 rounded bg-gray-300 px-4 py-2 text-gray-700 hover:bg-gray-400"
+              onClick={() => setAddingTask(false)}
+              className="w-1/2 rounded bg-gray-300 px-4 py-2 text-gray-700 hover:bg-gray-400 disabled:cursor-not-allowed"
+              disabled={addTaskMutation.isLoading}
             >
               Cancel
             </button>
@@ -65,7 +90,7 @@ const AddTask = ({ taskListId,userId }: TypeAddTaskProps) => {
       ) : (
         <button
           onClick={() => setAddingTask(true)}
-          className="w-full rounded border border-dashed border-blue-500 px-4 py-2 text-blue-500 hover:bg-gray-100 mt-4"
+          className="mt-4 w-full rounded border border-dashed border-blue-500 px-4 py-2 text-blue-500 hover:bg-gray-100"
         >
           + Add a task
         </button>
--- ui/AddTaskList.tsx
+++ ui/AddTaskList.tsx
@@ -1,20 +1,23 @@
 "use client"
-import React, { useRef, useState } from "react";
+import React, { useRef, useState ,useEffect} from "react";
 import api from "@trpc/client-side/react";
 import useSingleBoardState from "@store/useSingleBoardState";
 const AddTaskList = () => {
   const [addingTaskList, setAddingTaskList] = useState<boolean>(false);
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const addtaskListContainerRef =useRef<HTMLDivElement | null>(null);
   const addTaskListMutation = api.taskList.create.useMutation();
   const addTaskList = useSingleBoardState((state) => state.addTaskList);
   const handleAddTaskList = async () => {
     if (
       inputRef.current?.value === undefined ||
       !inputRef ||
-      inputRef.current.value.length === 0
+      inputRef.current.value.length === 0 || inputRef.current.value.length === 0 || !inputRef.current.value
     ) {
+        setAddingTaskList(false);
       return;
-    } else if (inputRef.current?.value && inputRef.current.value.length === 0) {
+    } 
+    else if (inputRef.current?.value && inputRef.current.value.length === 0) {
       setAddingTaskList(false);
     }
     await addTaskListMutation.mutateAsync(
@@ -29,11 +32,31 @@ const AddTaskList = () => {
       },
     );
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        addingTaskList &&
+        inputRef.current && addtaskListContainerRef.current &&
+        !inputRef.current.contains(event.target as Node) && 
+        !addtaskListContainerRef.current.contains(event.target as Node)
+      ) {
+        setAddingTaskList(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [addingTaskList]);
+
   return (
     <div>
       {" "}
       {addingTaskList ? (
-        <div className="rounded bg-gray-100 p-4 shadow">
+        <div className="rounded bg-gray-100 p-4 shadow" ref={addtaskListContainerRef}>
           <input
             type="text"
             placeholder="Enter task list title"
@@ -43,14 +66,15 @@ const AddTaskList = () => {
           <div className="flex space-x-2">
             <button
               onClick={handleAddTaskList}
-              className="w-1/2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 disabled:cursor-not-allowed"
+              className="w-1/2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 disabled:cursor-not-allowed disabled:opacity-50"
               disabled={addTaskListMutation.isLoading}
             >
               Add
             </button>
             <button
               onClick={() => setAddingTaskList(false)}
-              className="w-1/2 rounded bg-gray-300 px-4 py-2 text-gray-700 hover:bg-gray-400"
+              className="w-1/2 rounded bg-gray-300 px-4 py-2 text-gray-700 hover:bg-gray-400 disabled:cursor-not-allowed"
+              disabled={addTaskListMutation.isLoading}
             >
               Cancel
             </button>
