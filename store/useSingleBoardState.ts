a450b060a483894d9336962ea04783663a73ab71
Shubham Gehlot <shubhamgehlot675@gmail.com>
2023-11-11 13:08:36 +0530

    use zustand to manage global store

--- store/useSingleBoardState.ts
+++ store/useSingleBoardState.ts
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { create } from "zustand";
 import type { TypeTaskLists } from "@ui/SingleBoard";
 import type { TypeSingleTaskList } from "@ui/SingleTaskList";
@@ -11,13 +11,17 @@ type State = {
 
 const useSingleBoardState = create(
   combine({ board: [] as TypeTaskLists }, (set) => ({
+
     addInitialBoard: (board: State["board"]) => set({ board }),
+
     addTaskList: (taskList: TypeSingleTaskList) =>
       set((state) => ({ board: [...state.board, taskList] })),
+
     deleteTaskList: (taskListId: string) =>
       set((state) => ({
         board: state.board.filter((board) => board.taskListId !== taskListId),
       })),
+
     renameTaskList: (taskListId: string, title: string) =>
       set((state) => ({
         board: state.board.map((board) =>
@@ -26,6 +30,7 @@ const useSingleBoardState = create(
             : board,
         ),
       })),
+
     addTaskToTaskList: (taskListId: string, task: Task) =>
       set((state) => ({
         board: state.board.map((board) =>
@@ -34,6 +39,7 @@ const useSingleBoardState = create(
             : board,
         ),
       })),
+
     deleteTaskFromTaskList: (taskListId: string, taskId: string) =>
       set((state) => ({
         board: state.board.map((board) =>
@@ -47,20 +53,65 @@ const useSingleBoardState = create(
             : board,
         ),
       })),
-      renameTaskInTaskList: (taskListId: string, taskId: string, task: string) => set((state) => ({
+      
+    renameTaskInTaskList: (taskListId: string, taskId: string, task: string) =>
+      set((state) => ({
         board: state.board.map((board) =>
           board.taskListId === taskListId
             ? {
                 ...board,
                 tasks: board.tasks.map((t) =>
-                  t.taskId === taskId ? { ...t, task } : t
+                  t.taskId === taskId ? { ...t, task } : t,
                 ),
               }
-            : board
+            : board,
         ),
       })),
       
+    moveTaskBetweenList: (
+      fromTaskListId: string,
+      toTaskListId: string,
+      taskId: string,
+    ) =>
+      set((state) => {
+        const taskListFrom = state.board.find(
+          (board) => board.taskListId === fromTaskListId,
+        );
+        
+        const taskListTo = state.board.find(
+          (board) => board.taskListId === toTaskListId,
+        );
 
+        if (!taskListFrom || !taskListTo) {
+          // Handle the case where either the source or destination task list is not found
+          return state;
+        }
+
+        const task = taskListFrom.tasks.find((t) => t.taskId === taskId);
+
+        if (!task) {
+          // Handle the case where the task is not found in the source task list
+          return state;
+        }
+
+        const updatedBoard = state.board.map((board) => {
+          if (board.taskListId === fromTaskListId) {
+            return {
+              ...board,
+              tasks: board.tasks.filter((t) => t.taskId !== taskId),
+            };
+          }
+          if (board.taskListId === toTaskListId) {
+            return {
+              ...board,
+              tasks: [...board.tasks, task],
+            };
+          }
+          return board;
+        });
+
+        return { board: updatedBoard };
+      }),
   })),
 );
 
